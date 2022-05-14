db537463dba26f70a8ea7485404db6520af6a70f
jbouthem <jeanluc.bouthemy@comcast.net>
2022-05-14 08:41:17 -0700

    snapshot - 8

--- app/controllers/snapshots_controller.rb
+++ app/controllers/snapshots_controller.rb
@@ -1,24 +1,23 @@
 class SnapshotsController < ApplicationController
     before_action :logged_in_user, only: [:create, :destroy]
-    before_action :correct_user, only: :destroy
-  
+    # before_action :correct_user, only: :destroy
   
     def create
       @snapshot = current_user.snapshots.build(snapshot_params)
-      @snapshot.image.attach(params[:snapshot][:image])
+      # @snapshot.image.attach(params[:snapshot][:image])
       if @snapshot.save
         flash[:success] = "Snapshot created"
         redirect_to root_url
       else
-        @feed_items = current_user.feed.paginate(page: params[:page])
+        # @feed_items = current_user.feed.paginate(page: params[:page])
         render 'html_pages/home'
       end
     end
   
     def destroy
-      @snapshot.destroy
-      flash[:success] = "Snapshot has been deleted."
-      redirect_to request.referrer || root_url
+      # @snapshot.destroy
+      # flash[:success] = "Snapshot has been deleted."
+      # redirect_to request.referrer || root_url
     end
   
     private
@@ -27,8 +26,8 @@ class SnapshotsController < ApplicationController
       params.require(:snapshot).permit(:content, :image)
     end
   
-    def correct_user
-      @snaphot = current_user.snapshots.find_by(id: params[:id])
-      redirect_to root_url if @snapshot.nil?
-    end
+    # def correct_user
+    #   @snaphot = current_user.snapshots.find_by(id: params[:id])
+    #   redirect_to root_url if @snapshot.nil?
+    # end
 end
